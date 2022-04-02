e3da6f084e57be009af9e473ddadc77111792e8d
ramishdev <rhmi1243@gmail.com>
2022-04-02 21:34:06 +0500

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import * as ReactDOMClient from 'react-dom/client';
+
 import './index.css';
 import Admin from './Admin';
 import reportWebVitals from './reportWebVitals';
+
+//const container = document.getElementById('root');
+
+// Create a root.
+//const root = ReactDOMClient.createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <Admin />
+//   </React.StrictMode>
+// );
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +23,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
