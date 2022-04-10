099e53b7e92cd73cfff0512dfac83b4d03d6292c
ramishdev <rhmi1243@gmail.com>
2022-04-10 16:17:04 +0500

    Revert

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,69 +1,48 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import './Navbar.css';
 //import {useNavigate} from 'react-router-dom'
 
 import {Nav,NavDropdown,Container,Navbar} from 'react-bootstrap'
 import {Form,FormControl,Button} from 'react-bootstrap/'
-import { NavLink } from 'react-router-dom'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 const MyNavbar = () => {
     //const navigate = useNavigate()
-    const islogged = false
     function handleSubmit(e) {
         e.preventDefault();    
         console.log('You clicked submit.');
         //navigate('/products')
       }
-    return (islogged)?(
+    return(
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container fluid>
-                <Navbar.Brand href="/">Sentrend</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button onClick={(e) => handleSubmit(e)} variant="outline-success" >Search</Button>
-                    </Form>
-                    <Nav className="mx-auto" >
-                        <NavDropdown title={<i className="bi bi-person-fill" />} id="collasible-nav-dropdown" align="end">
-                            <NavLink to={"/Profile"}>
-                                <NavDropdown.Item>Profile</NavDropdown.Item>
-                            </NavLink>
-                            <NavDropdown.Item href="#action/3.2">Settings</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-                            
-                        </NavDropdown>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container fluid>
+            <Navbar.Brand href="/">Sentrend</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button onClick={(e) => handleSubmit(e)} variant="outline-success" >Search</Button>
+                </Form>
+                <Nav className="mx-auto" >
+                    <NavDropdown title={<i className="bi bi-person-fill" />} id="collasible-nav-dropdown" align="end">
+                    
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        
+                    </NavDropdown>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
         
-        </div>
-    ):(
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container fluid>
-                <Navbar.Brand href="/">Sentrend</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mx-auto" >
-                        <NavDropdown title={<i className="bi bi-person-fill" />} id="collasible-nav-dropdown" align="end">
-                            <NavLink to={"/Login"}>
-                                <NavDropdown.Item>Login</NavDropdown.Item>
-                            </NavLink>
-                        </NavDropdown>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
         </div>
     );
 }
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -11,15 +11,16 @@ import { NavLink } from 'react-router-dom'
 const axios = require('axios').default;
 
 
-const Sidebar = () => {
+const Sidebar = ({setloading}) => {
   const [trends,setTrends] = useState([])
-  const [isloading,setloading] = useState(false);
+  //const [isloading,setloading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     const fetchTrends = async () => {
-      setloading(true);
+      //setloading(true);
       try{
+<<<<<<< HEAD
         await axios.post('http://localhost:8000/api/trends/update_trends/',{
           auth: {
             username: 'arslan',
@@ -30,12 +31,15 @@ const Sidebar = () => {
           },
           signal: controller.signal
         });
+=======
+>>>>>>> parent of 7f6fb5e (update)
         const data = await axios.get('http://localhost:8000/api/trends', {
           params: {
             limit: 10
           },
           signal: controller.signal
         });
+<<<<<<< HEAD
         let newState = data.data.map((trend) => ({"trend_name":trend.trend_name,"max_results":10,"count":1}));
         await axios.post('http://localhost:8000/api/tweets/update_tweets/',{
           body:{
@@ -51,6 +55,8 @@ const Sidebar = () => {
           },
           signal: controller.signal
         });
+=======
+>>>>>>> parent of 7f6fb5e (update)
         console.log(data.data)
         setTrends(data.data);
         console.log(data);
@@ -58,11 +64,12 @@ const Sidebar = () => {
       catch(err){
         console.error(err.message);
       }
-      setloading(false);
+      //setloading(false);
     }
     fetchTrends();
     return () => controller?.abort();
   },[]);
+<<<<<<< HEAD
 
   return (isloading)?(
 
@@ -70,6 +77,9 @@ const Sidebar = () => {
 
   ):(
     
+=======
+  return (
+>>>>>>> parent of 7f6fb5e (update)
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
         <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
