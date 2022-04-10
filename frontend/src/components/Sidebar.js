97e271c8190f229f0b3dad4833b3374275b8ef2f
ramishdev <rhmi1243@gmail.com>
2022-04-10 16:18:37 +0500

    Stable

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -20,43 +20,12 @@ const Sidebar = ({setloading}) => {
     const fetchTrends = async () => {
       //setloading(true);
       try{
-<<<<<<< HEAD
-        await axios.post('http://localhost:8000/api/trends/update_trends/',{
-          auth: {
-            username: 'arslan',
-            password: '123'
-          },
-          body:{
-            crawler_id : 1
-          },
-          signal: controller.signal
-        });
-=======
->>>>>>> parent of 7f6fb5e (update)
         const data = await axios.get('http://localhost:8000/api/trends', {
           params: {
             limit: 10
           },
           signal: controller.signal
         });
-<<<<<<< HEAD
-        let newState = data.data.map((trend) => ({"trend_name":trend.trend_name,"max_results":10,"count":1}));
-        await axios.post('http://localhost:8000/api/tweets/update_tweets/',{
-          body:{
-            "query":[
-                {newState}
-            ]
-            ,
-            "crawler":{
-                "id":1,
-                "type":["batch"],
-                "duration":10
-            }
-          },
-          signal: controller.signal
-        });
-=======
->>>>>>> parent of 7f6fb5e (update)
         console.log(data.data)
         setTrends(data.data);
         console.log(data);
@@ -69,17 +38,7 @@ const Sidebar = ({setloading}) => {
     fetchTrends();
     return () => controller?.abort();
   },[]);
-<<<<<<< HEAD
-
-  return (isloading)?(
-
-    <div><h1>Loading...</h1></div>
-
-  ):(
-    
-=======
   return (
->>>>>>> parent of 7f6fb5e (update)
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
         <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
