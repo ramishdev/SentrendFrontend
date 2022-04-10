0997062da08d6030e1c64d45957551fe5655f1a2
ramishdev <rhmi1243@gmail.com>
2022-04-10 16:35:57 +0500

    Revert ".."
    This reverts commit d4043ceaea0e1d6bb27d0a4b61709c8ab6ddb196.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -20,43 +20,12 @@ const Sidebar = ({setloading}) => {
     const fetchTrends = async () => {
       //setloading(true);
       try{
-<<<<<<< HEAD
-=======
-        await axios.post('http://localhost:8000/api/trends/update_trends/',{
-          auth: {
-            username: 'arslan',
-            password: '123'
-          },
-          body:{
-            crawler_id : 0
-          },
-          signal: controller.signal
-        });
->>>>>>> parent of fbf3fd3 (update)
         const data = await axios.get('http://localhost:8000/api/trends', {
           params: {
             limit: 10
           },
           signal: controller.signal
         });
-<<<<<<< HEAD
-=======
-        let newState = data.data.map((trend) => ({"trend_name":trend.name,"max_results":10,"count":1}));
-        await axios.post('http://localhost:8000/api/tweets/update_tweets/',{
-          body:{
-            "query":[
-                {newState}
-            ]
-            ,
-            "crawler":{
-                "id":1,
-                "type":["batch"],
-                "duration":10
-            }
-          },
-          signal: controller.signal
-        });
->>>>>>> parent of fbf3fd3 (update)
         console.log(data.data)
         setTrends(data.data);
         console.log(data);
@@ -69,12 +38,6 @@ const Sidebar = ({setloading}) => {
     fetchTrends();
     return () => controller?.abort();
   },[]);
-<<<<<<< HEAD
-=======
-  if(isloading){
-    return <h1>Loading...</h1>;
-  }
->>>>>>> parent of fbf3fd3 (update)
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#333" backgroundColor="#f0f0f0">
