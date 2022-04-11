d260db9e3a3865e036f8868ad554d12db377ac1f
ramishdev <rhmi1243@gmail.com>
2022-04-11 17:34:24 +0500

    added css

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,17 +1,32 @@
 import React, {useContext} from 'react'
 import AuthContext from '../context/AuthContext'
+import '../css/login.css'
 
 const LoginPage = () => {
 
   let {loginUser} = useContext(AuthContext)
 
   return (
-    <div>
-        <form onSubmit = {loginUser}>
-            <input type="text" name = "username" placeholder="Enter Username" />  
-            <input type="password" name = "password" placeholder="Enter Password" />   
-            <input type="submit" />   
-        </form>
+    <div className="auth-wrapper">
+        <div className="auth-inner">
+            <form onSubmit = {loginUser}>
+                <h3>Login In</h3>
+                <div className="form-group mt-2">
+                    <label>Username</label>
+                    <input type="text" name = "username" className="form-control" placeholder="Enter Username" />
+                </div>
+                <div className="form-group mt-2">
+                    <label>Password</label>
+                    <input type="password" name = "password" className="form-control" placeholder="Enter Password" />
+                </div>
+                <button type="submit" className="btn mt-2 btn-primary btn-block">Submit</button>
+            </form>
+            {/*<form onSubmit = {loginUser}>
+                <input type="text" name = "username" placeholder="Enter Username" />  
+                <input type="password" name = "password" placeholder="Enter Password" />   
+                <input type="submit" />   
+            </form>*/}
+        </div>
     </div>
   )
 }
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,5 +1,6 @@
 import React, {useContext} from "react"
 import AuthContext from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 
@@ -7,15 +8,38 @@ import AuthContext from '../context/AuthContext'
 const RegisterPage = () => {
 
     let {registerUser} = useContext(AuthContext)
-
+    const navigate = useNavigate()
+    
     return (
-      <div>
-          <form onSubmit = {registerUser}>
+        <div className="auth-wrapper">
+            <div className="auth-inner">
+                <form onSubmit = {registerUser}>
+                        <h3>Sign Up</h3>
+                        
+                        <div className="form-group">
+                            <label>Enter Username</label>
+                            <input type="text" name = "username" className="form-control" placeholder="Enter Username" />
+                        </div>
+                        <div className="form-group mt-2">
+                            <label>Enter Email address</label>
+                            <input type="email" name = "email" className="form-control" placeholder="Enter Email" />
+                        </div>
+                        <div className="form-group mt-2">
+                            <label>Enter Password</label>
+                            <input type="password" name = "password" className="form-control" placeholder="Enter Password" />
+                        </div>
+                        <button type="submit" className="form-control btn btn-primary btn-block mt-2">Sign Up</button>
+                        <p className="forgot-password text-right">
+                            Already registered <button className="btn btn-primary btn-block" onClick={() => navigate('/login')}>sign in?</button>
+                        </p>
+                </form>
+          {/*<form onSubmit = {registerUser}>
               <input type="text" name = "username" placeholder="Enter Username" />  
               <input type="text" name = "email" placeholder="Enter Email" />  
               <input type="password" name = "password" placeholder="Enter Password" />   
               <input type="submit" />   
-          </form>
+          </form>*/}
+          </div>
       </div>
     )
   }
