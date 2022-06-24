154bb10d1b73466f62d7c8fcccf65a996fe3b6e1
ramishdev <rhmi1243@gmail.com>
2022-06-24 21:35:40 +0500

    update

--- frontend/src/components/Topics.js
+++ frontend/src/components/Topics.js
@@ -3,6 +3,7 @@ import WordCloud from 'react-d3-cloud';
 import axios from '../hooks/axios.js'
 import { useOutletContext } from "react-router-dom";
 
+import {Button} from 'react-bootstrap/'
 
 const fontSize = (word) => word.value / 50;
 const rotate = (word) => (word.value % 90) - 45;
@@ -12,32 +13,7 @@ function TopicsCloud({trend}){
     const [results, setResults] = useState()
     const [isloading,setloading] = useState(false);
     console.log(trend)
-    useEffect(() => {
-        const controller = new AbortController();
-        const fetchTopics = async () => {
-          setloading(true);
-          setResults()
-          try{
-            const data = await axios.get(trend?.url +"/topics/", {
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                signal: controller.signal
-            });
-            console.log(data?.data)
-            setResults(data?.data)            
-          }
-          catch(err){
-            console.error(err.message);
-            setResults({})
-          }
-          setloading(false);
-        }
-        if(trend){
-             fetchTopics();
-        }
-        return () => controller?.abort();
-    },[trend])
+ 
     const newData = (data) => { 
         return(
             data.map((item) => ({
@@ -46,20 +22,43 @@ function TopicsCloud({trend}){
             }))
         )
     };
-    if(isloading){
-        <>Loading</>
+    const fetchTopics = async () => {
+        setloading(true);
+        setResults()
+        const controller = new AbortController();
+        try{
+        const data = await axios.get(trend?.url +"topics/", {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            signal: controller.signal
+        });
+        console.log(data?.data)
+        setResults(data?.data)            
+        }
+        catch(err){
+        console.error(err.message);
+        setResults({})
+        }
+        setloading(false);
+    }
+    const fetch = async () => {
+        await fetchTopics()
     }
 
-    return (results && Object.keys(results).length > 0)?(
+    return (trend)?(
         <> 
+            <Button variant="outline-primary" onClick={fetch}>Get Topics</Button>
+            {(results && Object.keys(results).length > 0)?(
             <div className="border border-sky-500 " style={{width:'40rem'}}>
-            <WordCloud
-                data={newData(results)}
-                fontSize={fontSize}
-                rotate={0}
-                padding={2}
-            />
+                <WordCloud
+                    data={newData(results)}
+                    fontSize={fontSize}
+                    rotate={0}
+                    padding={2}
+                />
             </div>
+            ):(<></>)}
         </>
     ):
     (
